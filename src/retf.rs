7a8c8895e9f2bd2352dd83ba525c8b5422eb71d4
Leeb <leebdeveloper@gmail.com>
2013-09-22 13:53:10 +0400

    Development start

---
+++ src/retf.rs
@@ -0,0 +1,13 @@
+#[link(name = "retf",
+       vers = "0.1",
+       uuid = "7ab267cc-ec76-4af2-84e6-ee4c3696d000")];
+#[crate_type = "lib"];
+
+//use std::*;
+//use std::str::from_bytes;
+
+//use std::io::{WriterUtil,ReaderUtil};
+
+enum ErlangTerm {
+	String(@str)
+}
