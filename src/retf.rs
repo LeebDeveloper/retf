1ae8bc7b299d2fc83c0ae63dcf5a5b36debe1073
Leeb <leebdeveloper@gmail.com>
2013-09-23 23:17:08 +0400

    Atom cache ref parsing, some names refactor

--- src/retf.rs
+++ src/retf.rs
@@ -10,9 +10,10 @@ use std::str::from_chars;
 use std::f64::from_str;
 
 pub enum ErlangTerm {
+    Atom(@str),
+    AtomCacheRef(u8),
     Integer(i32),
     Float(f64),
-    Atom(@str),
     Reference(@ErlangTerm, @[u32], u8),
     Port(@ErlangTerm, u32, u8),
     Pid(@ErlangTerm, u32, u32, u8),
@@ -32,11 +33,53 @@ pub struct Decoder {
     reader: @io::Reader
 }
 
+pub struct Encoder {
+    writer: @io::Writer
+}
+
 impl Decoder {
     fn new(reader: @io::Reader) -> Decoder {
         Decoder {
             reader : reader
         }
+    }
+
+    fn decode(&self) -> @ErlangTerm {
+        let tag = self.reader.read_u8();
+        match tag {
+            82  => self.parse_atom_cache_ref(),
+            97  => self.parse_small_integer(),
+            98  => self.parse_integer(),
+            99  => self.parse_float(),
+            100 => self.parse_atom(false),
+            101 => self.parse_reference(),
+            102 => self.parse_port(),
+            103 => self.parse_pid(),
+            104 => self.parse_tuple(true),
+            105 => self.parse_tuple(false),
+            106 => @Nil, // No need to parse anything else
+            107 => self.parse_string(),
+            108 => self.parse_list(),
+            109 => self.parse_binary(),
+            110 => self.parse_biginteger(true),
+            111 => self.parse_biginteger(false),
+            114 => self.parse_newreference(),
+            115 => self.parse_atom(true),
+            117 => self.parse_fun(),
+            112 => self.parse_newfun(),
+            113 => self.parse_export(),
+            77  => self.parse_bitbinary(),
+            70  => self.parse_newfloat(),
+            118 => self.parse_atom(false),
+            119 => self.parse_atom(true),
+            131 => self.decode(), // Just term version, go ahead
+            _   => fail!("Unknown etf tag")
+        }        
+    }
+
+    priv fn parse_atom_cache_ref(&self) -> @ErlangTerm {
+        let v = self.reader.read_u8();
+        @AtomCacheRef(v)
     }
 
     priv fn parse_small_integer(&self) -> @ErlangTerm {
@@ -68,21 +111,21 @@ impl Decoder {
     }
 
     priv fn parse_reference(&self) -> @ErlangTerm {
-        let n = self.parse();
+        let n = self.decode();
         let i = self.reader.read_be_u32();
         let c = self.reader.read_u8();
         @Reference(n, @[i], c)
     }
 
     priv fn parse_port(&self) -> @ErlangTerm {
-        let n = self.parse();
+        let n = self.decode();
         let i = self.reader.read_be_u32();
         let c = self.reader.read_u8();
         @Port(n, i, c)
     }
 
     priv fn parse_pid(&self) -> @ErlangTerm {
-        let n = self.parse();
+        let n = self.decode();
         let i = self.reader.read_be_u32();
         let s = self.reader.read_be_u32();
         let c = self.reader.read_u8();
@@ -102,7 +145,7 @@ impl Decoder {
     priv fn parse_tuple(&self, small:bool) -> @ErlangTerm {
         let l = if small {self.reader.read_u8() as u32} 
                 else {self.reader.read_be_u32()};
-        let v = self._parse_vec(l, || self.parse());
+        let v = self._parse_vec(l, || self.decode());
         @Tuple(v)
     }
 
@@ -115,8 +158,8 @@ impl Decoder {
 
     priv fn parse_list(&self) -> @ErlangTerm {
         let l = self.reader.read_be_u32();
-        let v = self._parse_vec(l, || self.parse());
-        let r = match self.parse() {
+        let v = self._parse_vec(l, || self.decode());
+        let r = match self.decode() {
             @Nil => v,
             t    => at_vec::append(v, [t])
         };
@@ -140,7 +183,7 @@ impl Decoder {
     priv fn parse_newreference(&self) -> @ErlangTerm {
         let l = self.reader.read_be_u16() as u32;
         if l > 3 { fail!("Too long NewReference") };
-        let n = self.parse();
+        let n = self.decode();
         let c = self.reader.read_u8();
         let v = self._parse_vec(l, || self.reader.read_be_u32());
         @Reference(n, v, c)
@@ -148,11 +191,11 @@ impl Decoder {
 
     priv fn parse_fun(&self) -> @ErlangTerm {
         let l = self.reader.read_be_u32();
-        let p = self.parse();
-        let m = self.parse();
-        let i = self.parse();
-        let u = self.parse();
-        let v = self._parse_vec(l, || self.parse());
+        let p = self.decode();
+        let m = self.decode();
+        let i = self.decode();
+        let u = self.decode();
+        let v = self._parse_vec(l, || self.decode());
         let t = @[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
         @Fun(0, t, 0, l, m, i, u, p, v)
     }
@@ -163,18 +206,18 @@ impl Decoder {
         let u  = self._parse_vec(16, || self.reader.read_u8());
         let i  = self.reader.read_be_u32();
         let l  = self.reader.read_be_u32();
-        let m  = self.parse();
-        let oi = self.parse();
-        let ou = self.parse();
-        let p  = self.parse();
-        let v  = self._parse_vec(l, || self.parse());
+        let m  = self.decode();
+        let oi = self.decode();
+        let ou = self.decode();
+        let p  = self.decode();
+        let v  = self._parse_vec(l, || self.decode());
         @Fun(a, u, i, l, m, oi, ou, p, v)
     }
 
     priv fn parse_export(&self) -> @ErlangTerm {
-        let m = self.parse();
-        let f = self.parse();
-        let a = self.parse();
+        let m = self.decode();
+        let f = self.decode();
+        let a = self.decode();
         @Export(m, f, a)
     }
 
@@ -190,37 +233,20 @@ impl Decoder {
         @Float(f)
     }
 
-    fn parse(&self) -> @ErlangTerm {
-        let tag = self.reader.read_u8();
-        match tag {
-            97  => self.parse_small_integer(),
-            98  => self.parse_integer(),
-            99  => self.parse_float(),
-            100 => self.parse_atom(false),
-            101 => self.parse_reference(),
-            102 => self.parse_port(),
-            103 => self.parse_pid(),
-            104 => self.parse_tuple(true),
-            105 => self.parse_tuple(false),
-            106 => @Nil, // No need to parse anything else
-            107 => self.parse_string(),
-            108 => self.parse_list(),
-            109 => self.parse_binary(),
-            110 => self.parse_biginteger(true),
-            111 => self.parse_biginteger(false),
-            114 => self.parse_newreference(),
-            115 => self.parse_atom(true),
-            117 => self.parse_fun(),
-            112 => self.parse_newfun(),
-            113 => self.parse_export(),
-            77  => self.parse_bitbinary(),
-            70  => self.parse_newfloat(),
-            118 => self.parse_atom(false),
-            119 => self.parse_atom(true),
-            _   => fail!("Unknown etf tag")
-        }        
+    priv fn parse_distribution_header(&self) -> @[@ErlangTerm] {
+        @[]
     }
     
+}
+
+impl Encoder {
+    fn new(writer: @io::Writer) -> Encoder {
+        Encoder {
+            writer: writer
+        }
+    }
+
+
 }
 
 // ---------------------
@@ -234,7 +260,7 @@ mod tests {
     fn test_parse(bytes: &[u8], f: &fn(@ErlangTerm)) {
         io::with_bytes_reader(bytes, |rd| {
             let decoder = Decoder::new(rd);
-            f(decoder.parse())
+            f(decoder.decode())
         });
     }
 
@@ -358,6 +384,16 @@ mod tests {
             |i| {
             match i {
                 @Reference(@Atom(string), [188,0,0], 0) => assert_eq!(string, @"nonode@nohost"),
+                _ => fail!()
+            }});
+    }
+
+    #[test]
+    fn deserialize_full_test() {
+        test_parse([131,100,0,2,111,107], 
+            |i| {
+            match i {
+                @Atom(string) => assert_eq!(string, @"ok"),
                 _ => fail!()
             }});
     }
